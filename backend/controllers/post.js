d6940396fad051baff8cb17f166a42d59493ff2e
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2023-07-02 22:12:51 +0200

    added uploading img funcionality

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -12,6 +12,7 @@ export const getPosts = (req, res) => {
         return res.status(200).json(data);
     })
 }
+
 export const getPost = (req, res) => {
   const q =
     "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
@@ -22,9 +23,11 @@ export const getPost = (req, res) => {
     return res.status(200).json(data[0]);
   });
 };
+
 export const addPost = (req, res) => {
     res.json('from controller')
 }
+
 export const deletePost = (req, res) => {
     const token = req.cookies.access_token
     if(!token) return res.status(401).json('Not authorized');
@@ -42,6 +45,7 @@ export const deletePost = (req, res) => {
         })
     })
 }
+
 export const updatePost = (req, res) => {
     res.json('from controller')
 }
--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,30 @@ import postRoutes from './routes/posts.js'
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
 import cookieParser from "cookie-parser"
+import multer from "multer"
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, img, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, img, cb) {
+    cb(null, Date.now() + img.originalname);
+  },
+});
+
+const upload = multer({ storage})
+
+
+app.post('/api/upload', upload.single('img'), function (req, res) {
+    const file = req.file;
+res.status(200).json(file.filename)
+})
+
 app.use('/api/posts', postRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
