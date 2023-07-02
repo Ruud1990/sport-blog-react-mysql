9ba3a91dfa5f8ef1c7dd463fa2b3ba359ad1677d
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2023-07-02 20:32:38 +0200

    fetching recommended posts

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const Menu = () => {
-    const posts = [
-    {
-      id: 1,
-      title: 'lorem ipsum',
-      desc: 'lorem ipsum',
-      img: 'https://media.istockphoto.com/id/608516034/pl/zdj%C4%99cie/koncepcja-fitness-i-treningu-wellness.jpg?s=2048x2048&w=is&k=20&c=PkgLkngAYhfNTP6OpFG4tYSbsq8ne7zAJ6Bh4ECVDNg='
-    },
-    {
-      id: 2,
-      title: 'lorem ipsum',
-      desc: 'lorem ipsum',
-      img: 'https://media.istockphoto.com/id/608516034/pl/zdj%C4%99cie/koncepcja-fitness-i-treningu-wellness.jpg?s=2048x2048&w=is&k=20&c=PkgLkngAYhfNTP6OpFG4tYSbsq8ne7zAJ6Bh4ECVDNg='
-    },
-    {
-      id: 3,
-      title: 'lorem ipsum',
-      desc: 'lorem ipsum',
-      img: 'https://media.istockphoto.com/id/608516034/pl/zdj%C4%99cie/koncepcja-fitness-i-treningu-wellness.jpg?s=2048x2048&w=is&k=20&c=PkgLkngAYhfNTP6OpFG4tYSbsq8ne7zAJ6Bh4ECVDNg='
-    },
-    {
-      id: 4,
-      title: 'lorem ipsum',
-      desc: 'lorem ipsum',
-      img: 'https://media.istockphoto.com/id/608516034/pl/zdj%C4%99cie/koncepcja-fitness-i-treningu-wellness.jpg?s=2048x2048&w=is&k=20&c=PkgLkngAYhfNTP6OpFG4tYSbsq8ne7zAJ6Bh4ECVDNg='
-    }
-  ]
+const Menu = ({cat}) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/?cat=${cat}`)
+        setPosts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [cat]);
+  //   const posts = [
+  //   {
+  //     id: 1,
+  //     title: 'lorem ipsum',
+  //     desc: 'lorem ipsum',
+  //     img: 'https://media.istockphoto.com/id/608516034/pl/zdj%C4%99cie/koncepcja-fitness-i-treningu-wellness.jpg?s=2048x2048&w=is&k=20&c=PkgLkngAYhfNTP6OpFG4tYSbsq8ne7zAJ6Bh4ECVDNg='
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'lorem ipsum',
+  //     desc: 'lorem ipsum',
+  //     img: 'https://media.istockphoto.com/id/608516034/pl/zdj%C4%99cie/koncepcja-fitness-i-treningu-wellness.jpg?s=2048x2048&w=is&k=20&c=PkgLkngAYhfNTP6OpFG4tYSbsq8ne7zAJ6Bh4ECVDNg='
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'lorem ipsum',
+  //     desc: 'lorem ipsum',
+  //     img: 'https://media.istockphoto.com/id/608516034/pl/zdj%C4%99cie/koncepcja-fitness-i-treningu-wellness.jpg?s=2048x2048&w=is&k=20&c=PkgLkngAYhfNTP6OpFG4tYSbsq8ne7zAJ6Bh4ECVDNg='
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'lorem ipsum',
+  //     desc: 'lorem ipsum',
+  //     img: 'https://media.istockphoto.com/id/608516034/pl/zdj%C4%99cie/koncepcja-fitness-i-treningu-wellness.jpg?s=2048x2048&w=is&k=20&c=PkgLkngAYhfNTP6OpFG4tYSbsq8ne7zAJ6Bh4ECVDNg='
+  //   }
+  // ]
   return (
     <div className='menu'>
         <h1>Other post You may like</h1>
